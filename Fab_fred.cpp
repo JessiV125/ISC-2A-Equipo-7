aefa2d66d056a141d2f82409abd8b3274d2bd72b
jeesiv125 <vane.mdrosa@gmail.com>
2025-04-09 08:42:18 -0600

    Cambio en el main

--- Fab_fred.cpp
+++ Fab_fred.cpp
@@ -12,8 +12,23 @@ Proyecto FRED
 
 #include <iostream>
 using namespace std;
-  
+
+void MostrarMenu();
+
 int main(){
   cout << "Proyecto FRED";
+  MostrarMenu();
+
   return 0;
+}
+
+void MostrarMenu(){
+  int opc;
+
+  cout << "Menu";
+  cout << "1 - Opcion";
+  cout << "2 - Opcion";
+  cout << "Ingresa una opcion: ";
+  cin >> opc;
+  
 }
