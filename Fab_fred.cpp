c2898027997e4c0182b62fbfb5f0e238ab8e73cb
Val-264 <al343508@edu.uaa.mx>
2025-04-13 13:40:36 -0600

    reglas

--- Fab_fred.cpp
+++ Fab_fred.cpp
@@ -41,12 +41,14 @@ void realizarRegistro(){
 void elegirNivel(){
   int opc=0;
   float opcion=0;
+  cout<<"\nEste es un juego para trabajar tu memeoria:\nSe mostrara en pantalla una secuencia de colores en una tabla\nla cual deberas memorizar y replicar posteriormente.\n\n";
   do{
-    cout<<"\nElige una opcion:";
     cout<<"\nNivel 1......1";
     cout<<"\nNivel 2......2";
     cout<<"\nNivel 3......3";
     cout<<"\nSalir........4";
+    cout<<"\nElige una opcion: ";
+    cin>>opcion;
     if(cin.fail()){ // Si la entrada no es un numero
       cin.clear(); // Limpiar estado de error de cin
       cin.ignore(1000,'\n'); // Descartar entrada inválida hasta mil caracteres o hasta encontrar un salto de línea
@@ -54,10 +56,10 @@ void elegirNivel(){
     else if(fmod(opcion,1)!=0) cout<<" "; // Descartar numeros con decimales 
     else opc=static_cast<int>(opcion); // convertir opcion a entero para que pueda ser evaluado por switch
     switch(opc){
-          case 1: jugarNivel1();
-          case 2: jugarNivel2();
-          case 3: jugarNivel3();
-          case 4: menuOpciones();
+          case 1: jugarNivel1(); break;
+          case 2: jugarNivel2(); break;
+          case 3: jugarNivel3(); break;
+          case 4: menuOpciones(); break;
           default: cout<<"Ups! esa opcion es invalida, intenta de nuevo";
     }
   }while(opc!=4);
