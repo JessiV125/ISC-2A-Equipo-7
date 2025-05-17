8e1b7a8f5240a32ed3df781a7359fd085a5e1ea3
Val-264 <al343508@edu.uaa.mx>
2025-05-17 14:48:58 -0600

    Funciones necesarias para jugar

--- Fab_fred.cpp
+++ Fab_fred.cpp
@@ -19,16 +19,36 @@ static int tope = 10;    //Contador  y declaración de la variable estatica
 // Obtener manejador de consola
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); 
 
-//CONSTANTES
-
-//COLORES
-enum{AZUL=1, VERDE, CYAN, ROJO, MAGENTA, AMARILLO};
-
-//STRUCT
+// ESTRUCTURAS
 struct Usuario{
     char nombre[10];
     char contrasena[20];
 };
+
+struct Celda{
+	int numero;
+	bool encendida;	
+};
+
+// VARIABLES GLOBALES
+Celda **tablero=NULL;
+int tamTablero;
+int secuenciaMaxima=0; // Cantidad de veces que se repite la secuencia a memorizar en cada nivel
+int puntaje=0;
+bool juegoActivo=true; // Interrupor: determina si el juego continua o no dependiendo de si el jugador repite la secuencia correctamente
+
+/* COORD es una estructura de datos definida en la API de windows  |  typedef struct _COORD{
+								que representa coordenadas en una pantalla de consola, en este 	|			SHORT X; //Coordenada horizontal (columna)
+								programa se usa para manejar posiciones en el tablero 			|		SHORT Y; //Coordenada vertical (fila)
+																								|	} COORD;												*/
+COORD posicionJugador={0,0}; // Posicion inicial del jugador: esquina superior izquierda
+COORD *secuencia=NULL; // Almacena las coordenadas de la secuencia a memorizar
+int longitudSecuencia=1; // Numero de casillas que se encienden por secuencia, aumenta si el jugador rpepite la secuencia correctamente
+int pasoActual=0; // Determina el elemento de la secuencia que el jugador debe repetir
+
+
+// COLORES
+enum{AZUL=1, VERDE, CYAN, ROJO, MAGENTA, AMARILLO};
 
 // PROTOTIPOS
 void limpiarPantalla();
@@ -51,6 +71,7 @@ bool verificarSeleccion();
 void eliminarTablero();
 
 int main(){
+  srand(time(NULL));
   cout << "Proyecto FRED";
   MostrarMenu();
   Usuario usuarios[10]; //Arreglo de usuarios inicializado
@@ -123,6 +144,9 @@ void realizarRegistro(){
 
 }
 
+
+// FUNCIONES PARA JUGAR
+
 /*
   -Explica reglas del juego
   -Tiene un menú y da a elegir entre 3 niveles
@@ -171,4 +195,314 @@ void elegirNivel(){
           default: cout<<"Ups! esa opcion es invalida, intenta de nuevo";
     	}
 	}while(opc!=4);
+}
+
+// Hace llamdas a otras funciones para crear el tablero y jugar
+void jugarNiveles(int nivel){	
+	// Determinar tamaño del tablero en base al nivel
+	if(nivel==1){
+		tamTablero=3;
+		secuenciaMaxima=10;
+		secuencia=new COORD[secuenciaMaxima];
+	} 
+	else if(nivel==2){
+		tamTablero=5;
+		secuenciaMaxima=15;	
+		secuencia=new COORD[secuenciaMaxima];
+	} 
+	else if(nivel==3){
+		tamTablero=7;
+		secuenciaMaxima=20;	
+		secuencia=new COORD[secuenciaMaxima];
+	} 
+
+	//Reiniciar estado para nuevo juego nuevo nivel
+	longitudSecuencia=1;
+	puntaje=0; 
+	juegoActivo=true;
+	pasoActual=0;
+	
+	// Inicializar tablero
+	eliminarTablero(); 
+	crearTablero();
+	inicializarTablero();
+	
+	while(juegoActivo){
+		// Reiniciar estado para nuevo nivel
+		posicionJugador={0,0};
+		pasoActual=0;
+		
+		generarSecuencia();
+		
+		if(!juegoActivo) break; // Terminar el juego si el jugador no repite la secuencia correctamente
+		
+		mostrarSecuencia();
+		dibujarTablero();
+		
+		bool secuenciaCompleta=false; // indica si la secuencia se repitió correctamente
+		while(!secuenciaCompleta && juegoActivo){ // Mientras la secuencia no llegue al maximo y el jugador no se equivoque al repetirla
+			if(_kbhit()){ // _kbhit() verifica si hay una entrada del teclado
+				int tecla=_getch(); // _getch() lee la tecla y obtiene su código. Si no se usa después de _kbhit() la tecla queda en el buffer
+				
+				if(tecla==0xE0){ // La primer llamada a _getch() retorna 0xE0 que indica que se presionó una tecla especial (flechas, inicio...)
+					tecla=_getch(); // La segunda llamada a _getch() retorna el código real de la tecla	
+					moverJugador(tecla);
+					dibujarTablero();
+				} 	
+				
+				else if(tecla==13){ // 13 es el código ASCII para la tecla Enter
+						secuenciaCompleta=verificarSeleccion();
+						dibujarTablero();
+						
+						if(secuenciaCompleta){
+							cout<<"Correcto! Puntaje: "<<puntaje<<endl;
+							cout<<"Secuencia completada correctamente.\n";
+							if(longitudSecuencia<secuenciaMaxima){
+								cout<<"Preparate para la siguiente secuencia";	
+							}
+							Sleep(3000);
+						}
+				}
+			}
+		}
+	}
+	
+	// AL FINALIZAR EL JUEGO:
+	system("cls");
+	int opc=0;
+	float opcion=0;
+	if(longitudSecuencia<secuenciaMaxima){ // Si no completa el nivel
+		do{
+			cout<<"\nSecuencia incorrecta. Fin del juego";
+			cout<<"\nPuntaje alcanzado: "<<puntaje;
+			cout<<"\nSecuencia alcanzada: "<<longitudSecuencia-1;
+			cout<<"\nRepetir nivel.........1";
+			cout<<"\nRegresar..............2";
+			cout<<"\nTu eleccion: ";
+			cin>>opcion;
+			if(cin.fail()){
+				cin.clear();
+				cin.ignore(1000,'\n');
+				system("cls");
+				cout<<"Entrada invalida";
+				Sleep(2000);
+				system("cls");
+			}
+			else if(fmod(opcion,1)==0) opc=static_cast<int>(opcion);
+			switch(opc){
+				case 1: jugarNiveles(nivel); return; 
+				case 2: eliminarTablero();
+						elegirNivel(); return;
+			}
+		}while(opc!=1 || opc!=2);
+	}
+	
+	else if(nivel==1 || nivel==2){ // Si completa el nivel 1 o 2 
+		do{
+			cout<<"\nNivel completado";
+			cout<<"\nPuntaje alcanzado: "<<puntaje;
+			cout<<"\nPasar al siguiente nivel.........1";
+			cout<<"\nRegresar.........................2";
+			cout<<"\nTu eleccion: ";
+			cin>>opcion;
+			if(cin.fail()){
+				cin.clear();
+				cin.ignore(1000,'\n');
+				system("cls");
+				cout<<"Entrada invalida";
+				Sleep(2000);
+				system("cls");
+			}
+			else if(fmod(opcion,1)==0) opc=static_cast<int>(opcion);
+			else{
+				system("cls");
+				cout<<"Entrada invalida";
+				Sleep(2000);
+				system("cls");	
+			}
+			switch(opc){
+				case 1: eliminarTablero();
+						jugarNiveles(nivel+1); return;  // Siguiente nivel
+				case 2: eliminarTablero();
+						elegirNivel(); return;
+			}
+		}while(opc!=1 || opc!=2);
+	}
+	
+	else{ // Si completa el nivel 3
+		do{
+			cout<<"\nNivel completado";
+			cout<<"\nPuntaje alcanzado: "<<puntaje;
+			cout<<"\nRegresar.............1";
+			cout<<"\nTu eleccion: ";
+			cin>>opcion;
+			if(cin.fail()){
+				cin.clear();
+				cin.ignore(1000,'\n');
+				system("cls");
+				cout<<"Entrada invalida";
+				Sleep(2000);
+				system("cls");
+			}
+			else if(opcion==1){
+				eliminarTablero();
+				elegirNivel();
+				return;
+			} 
+			else{
+				system("cls");
+				cout<<"Entrada invalida";
+				Sleep(2000);
+				system("cls");	
+			}
+		}while(opcion!=1);
+	}
+}
+
+
+/* Crea el tablero con diferentes tamaños
+ dependiendo del nivel que se juega */
+void crearTablero(){	
+	tablero=new Celda*[tamTablero]; // Renglones 
+	for(int i=0; i<tamTablero; i++){ // Columnas
+		tablero[i]=new Celda[tamTablero];
+	}
+}
+
+// Inicializa el tablero
+void inicializarTablero(){
+	for(int i=0; i<tamTablero; i++){
+		for(int j=0; j<tamTablero; j++){
+			tablero[i][j].numero=i*tamTablero+j+1;
+			tablero[i][j].encendida=false;
+		}
+	}	
+}
+
+// Genera la(s) posicione(s) de la(s) casilla(s) a encender
+void generarSecuencia(){
+	if(longitudSecuencia>secuenciaMaxima){ // Terminar juego si se repite toda la secuencia
+		juegoActivo=false; 
+		return;
+	}		
+	
+	
+	for(int i=0; i<longitudSecuencia; i++){ // Posicion de la casilla a encender 
+		bool repetida;
+		do{
+			repetida=false;
+			secuencia[i].X=rand()%tamTablero;
+			secuencia[i].Y=rand()%tamTablero;
+			
+			/* Verificar que la secuencia sea única para evitar 
+			problemas en el seguimiento de la secuencia */
+			for(int j=0; j<i; j++){
+				if(secuencia[i].X == secuencia[j].X && secuencia[i].Y == secuencia[j].Y) repetida=true; break;
+			}
+		}while(repetida);	
+	}	
+}
+
+// Enciende las celdas y llama a dibujarTablero
+void mostrarSecuencia(){
+	system("cls");	
+	for(int i=0; i<longitudSecuencia; i++){
+		tablero[secuencia[i].Y][secuencia[i].X].encendida=true;
+		dibujarTablero();
+		Sleep(1000);
+		tablero[secuencia[i].Y][secuencia[i].X].encendida=false;
+		dibujarTablero();
+		Sleep(300);		
+	}
+}
+
+/* Dibuja el tablero para mostrar la secuencia a repetir
+	o la posicion en la que se ubica el jugador para 
+	repetir la secuencia*/
+void dibujarTablero(){
+	system("cls");
+	// Encabezado
+	if(secuenciaMaxima==10) cout<<"\tNIVEL 1"<<endl;
+	else if(secuenciaMaxima==15) cout<<"\tNIVEL 2"<<endl;
+	else cout<<"\tNIVEL 3"<<endl;
+	cout<<"Puntaje: "<<puntaje<<"  |  Secuencia: "<<longitudSecuencia<<"/"<<secuenciaMaxima<<endl<<endl;
+	
+	for(int i=0; i<tamTablero; i++){
+		for(int j=0; j<tamTablero; j++){
+			// Mostrar secuencia
+			if(tablero[i][j].encendida){
+				int color=1+(rand()%6); // Generar color para las casillas
+				switch(color){
+					case 1: SetConsoleTextAttribute(hConsole, AZUL); break;
+					case 2: SetConsoleTextAttribute(hConsole, VERDE); break;
+					case 3: SetConsoleTextAttribute(hConsole, CIAN); break;
+					case 4: SetConsoleTextAttribute(hConsole, ROJO); break;
+					case 5: SetConsoleTextAttribute(hConsole, MAGENTA); break;
+					case 6: SetConsoleTextAttribute(hConsole, AMARILLO); break;
+				}
+				cout<<"[";
+				if(tablero[i][j].numero<10) cout<<" ";
+				cout<<tablero[i][j].numero<<"]";
+			}
+			
+			// Mostrar posición del jugador en el tablero
+			else{
+				SetConsoleTextAttribute(hConsole, BLANCO);
+				cout<<"[";
+				if(posicionJugador.X==j && posicionJugador.Y==i){ // Casilla en la que se ubica el jugador
+					SetConsoleTextAttribute(hConsole, 240); // Fondo blanco, texto negro
+					cout<<" X";
+				}
+				else cout<<"  "; // Casillas en las que no está el jugador
+				cout<<"]";
+			}
+			SetConsoleTextAttribute(hConsole, BLANCO);
+		}
+		cout<<endl;
+	}
+	
+}
+
+// Mueve al jugador en el tablero dependiendo de la flecha preionada en teclado
+void moverJugador(int tecla){
+	switch(tecla){
+		case 75: if(posicionJugador.X>0) posicionJugador.X--; break; 			// Izquierda
+		case 77: if(posicionJugador.X<tamTablero-1) posicionJugador.X++; break; // Derecha
+		case 72: if(posicionJugador.Y>0) posicionJugador.Y--; break; 			// Arriba
+		case 80: if(posicionJugador.Y<tamTablero-1) posicionJugador.Y++; break; // Abajo
+	}	
+}
+
+bool verificarSeleccion(){
+	if(posicionJugador.X==secuencia[pasoActual].X && posicionJugador.Y==secuencia[pasoActual].Y){
+		
+		pasoActual++;
+		
+		if(pasoActual>=longitudSecuencia){
+			puntaje+=longitudSecuencia*10;
+			longitudSecuencia++;
+			pasoActual=0;
+			return true;
+		}
+	}	
+	else juegoActivo=false;
+	return false;
+}
+
+/* Libera el espacio en memoeria que ocupa el tablero actual
+	para crear otro con otro tamaño dependiendo del nivel
+	que el usuario desee jugar
+*/
+void eliminarTablero(){
+	if(tablero!=NULL){ 
+		// Eliminar columnas
+		for(int i=0; i<tamTablero; i++){
+			delete[] tablero[i]; 
+		}
+	
+		// Eliminar filas
+		delete[] tablero;
+		
+		tablero=NULL;		
+	}
 }
